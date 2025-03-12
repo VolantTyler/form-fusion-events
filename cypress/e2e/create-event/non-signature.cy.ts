df93eb1cfdcb23c315f4f90f86878b839cb50d45
Tyler Stahl <tstahl@charitynavigator.org>
2025-03-12 10:20:02 -0400

    feat: enhance Cypress tests for event creation and improve data-testid attributes

--- cypress/e2e/create-event/non-signature.cy.ts
+++ cypress/e2e/create-event/non-signature.cy.ts
@@ -1,21 +1,22 @@
-describe('template spec', () => {
+describe('non-signature event spec', () => {
   it('fails to find a non-existent element', function() {
     cy.visit('localhost:8080');
     cy.get('#non-existent-element').should('exist');
   });
 
   it('creates a non-signature event', function() {
-    cy.visit('localhost:8080');
+    cy.visit('http://localhost:8080/');
+    cy.get('.flex > .transition-colors').click();
     cy.get('#radix-\\:r0\\:-trigger-affiliate').click();
-    cy.get('.text-center > .inline-flex').click();
-    cy.get('#\\:r4\\:-form-item').type('Affiliate Conf 2025');
-    cy.get('#\\:r5\\:-form-item').clear();
-    cy.get('#\\:r5\\:-form-item').type('Nebraska');
-    cy.get('#\\:r6\\:-form-item').click();
-    cy.get(':nth-child(4) > :nth-child(5) > .rdp-button_reset').click();
-    cy.get('#\\:r8\\:-form-item > .flex').clear('2');
-    cy.get('#\\:r8\\:-form-item > .flex').type('3');
-    cy.get('.bg-primary').click();
-    cy.get('.text-2xl').should('have.text', 'Affiliate Conf 2025');
+    cy.get('[data-testid="create-affiliate-event"]').click();
+    cy.get('[data-testid="event-name"]').clear('A');
+    cy.get('[data-testid="event-name"]').type('Affiliate Conf 2024');
+    cy.get('[data-testid="event-location"]').clear();
+    cy.get('[data-testid="event-location"]').type('There');
+    cy.get('[data-testid="event-date"]').click();
+    cy.get(':nth-child(2) > :nth-child(6) > .rdp-button_reset').click();
+    cy.get('[data-testid="submit-event"]').click();
+    cy.get('.text-2xl').should('have.text', 'Affiliate Conf 2024');
+    cy.get('.space-y-1\\.5 > .inline-flex').should('be.visible');
   });
 })
